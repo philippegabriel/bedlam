d70f01883e52b5a263e80d9e2ab77118e76bf1f2
philippe gabriel <philippe.gabriel@citrix.com>
2014-03-21 14:03:05 +0000

    fixing emitting smt2, added merging with bedlam.smt2.in

--- xyzList2vector.pl
+++ xyzList2vector.pl
@@ -8,6 +8,35 @@
 # input on stdin, output on stdout
 #
 #Convert from a List to an smt2 fun
+
+#
+#emit define-fun for vector 
+#
+sub emitVector{
+my ($name,$vector,$rank)=@_;
+$padding=100-length($vector);
+print '(define-fun '.$name.'_'.$rank.' () (_ BitVec 100) #b'.$vector.'0' x $padding.")\n";
+}
+#
+#emit mux function associated with vector
+#
+sub emitMuxFun{
+my ($name,$len)=@_;
+#if we moved to the next set of vector, emit the mux function
+print ';Pick '.$name."\n";
+print '(define-fun '.$name." ((x (_ BitVec 5))) (_ BitVec 100)\n";
+foreach(0..$len)
+	{printf("(ite (= x (_ bv%d 5)) %s_%d\n",$_,$name,$_);}
+print 'empty';
+foreach(0..$len)
+	{print ')';}	#close the ite block
+print ")\n";		#close the define-fun	
+#emit the range of the mux function
+print '(assert (bvule '.$name.'xyz (_ bv'.$len." 5 )))\n";
+}
+#
+#main
+#
 $prevName='';
 while(<>)
 {
@@ -18,22 +47,12 @@ if($prevName eq $name)
 	{$i++;}
 else
 	{if($prevName ne '')
-#if we moved to the next set of vector, emit the mux function
-		{print ';Pick '.$prevName."\n";
-		print '(define-fun '.$prevName." ((x (_ BitVec 5))) (_ BitVec 100)\n";
-		foreach(0..$i)
-			{printf("(ite (= x (_ bv%d 5)) %s_%d\n",$_,$prevName,$_);}
-		foreach(0..$i)
-			{print ')';}
-		print ")\n";
-#emit the range of the mux function
-		print '(assert (bvule '.$prevName.'xyz (_ bv'.$i." 5 )))\n";
-		}
+		{emitMuxFun($prevName,$i);}
 	$i=0;
 	}
-
-#emit the bin vector
-print '(define-fun '.$name.'_'.$i.' () (_ BitVec 100) #b'.$vector.")\n";
+emitVector($name,$vector,$i);
 $prevName=$name;
 }
+emitMuxFun($prevName,$i);
+
 
